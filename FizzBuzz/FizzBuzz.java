8c2e19d630fed8b18022fc86453f60a20537539a
bobend212 <35953089+bobend212@users.noreply.github.com>
2018-09-21 11:44:28 +0200

    Upload

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,1 +1,30 @@
+package fizzbuzz;
 
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("How many numbers do you want display? (insert) ");
+
+        int number = inp.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+
+            if (i%3 == 0 && i%5 ==0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i%3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i%5 == 0) {
+                System.out.println("Buzz");
+            }
+            else
+                System.out.println(i);
+        }
+    }
+}
